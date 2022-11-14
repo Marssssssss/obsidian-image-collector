b93421aa529cba3efcb76c8c9355071729588b5f
Marssssssss <3503715604@qq.com>
2022-11-15 00:31:49 +0800

    remove out dir

--- out/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reg_exp_escape = void 0;
-function reg_exp_escape(raw_str) {
-    return raw_str.replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, '\\$&');
-}
-exports.reg_exp_escape = reg_exp_escape;
